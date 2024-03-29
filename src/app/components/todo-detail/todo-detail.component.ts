aab5751555aeec5a1a69529898fc26ecc5f20b27
Michael Harlev Kristensen <MicHarKris@gmail.com>
2024-03-29 19:44:39 +0100

    Details View
    Working details view, with proper formatting and styling

--- src/app/components/todo-detail/todo-detail.component.ts
+++ src/app/components/todo-detail/todo-detail.component.ts
@@ -1,17 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-// import { TodoService } from '../../services/todo.service';
-// import { Todo } from '../../models/todo.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-todo-detail',
   standalone: true,
+  imports: [
+    CommonModule, 
+    HttpClientModule
+  ],
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.css']
 })
-export class TodoDetailComponent {
 
+export class TodoDetailComponent implements OnInit {
+  todo: any;
 
-  
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) {}
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      const todoId = paramMap.get('id');
+      if (todoId) {
+        this.fetchTodoDetail(+todoId);
+      }
+    });
+  }
+
+  fetchTodoDetail(id: number): void {
+    this.httpClient.get<any[]>('https://boyumcodechallenge.azurewebsites.net/api/todolist')
+      .subscribe(todoList => {
+        this.todo = todoList.find(todo => todo.Id === id);
+        if (this.todo && typeof this.todo.Created === 'number') {
+          this.todo.Created = new Date(this.todo.Created);
+
+        }
+      });
+  }
+
+  isValidDate(date: any): boolean {
+    return date instanceof Date && !isNaN(date.getTime());
+  }
+
+  getDaysOld(createdDate: Date): number {
+    const today = new Date();
+    const difference = today.getTime() - createdDate.getTime();
+    return Math.floor(difference / (1000 * 60 * 60 * 24));
+  }
 }
